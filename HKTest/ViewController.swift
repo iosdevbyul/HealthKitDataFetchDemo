d59e6fc96903c8d603263367bdeb08fd43fd802d
Jessi Ju <sbjoo@pntbiz.com>
2023-10-13 13:42:55 +0900

    Initial Commit

---
+++ HKTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HKTest
+//
+//  Created by PNT001 on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
